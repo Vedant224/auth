d76742c56be82c6eb6eb5b0c836eeb267435d171
vedant <vedanthande2244@gmail.com>
2025-07-17 18:09:14 +0530

    adding trusthost

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,12 @@
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
-import NextAuth from "next-auth/next";
+import type { NextAuthOptions } from "next-auth";
 
-const handler = NextAuth(authOptions);
+const extendedAuthOptions: NextAuthOptions & { trustHost: boolean } = {
+  ...authOptions,
+  trustHost: true,
+};
 
-export {handler as GET ,handler as POST}
+const handler = NextAuth(extendedAuthOptions);
+
+export { handler as GET, handler as POST };
